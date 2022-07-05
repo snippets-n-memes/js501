249f66e5dba5127dbf18717168cbf3b937c02586
mbmcmullen27 <mbmcmullen27@gmail.com>
2022-07-05 18:22:56 -0500

    modify class prototype

---
+++ classes.js
@@ -0,0 +1,37 @@
+class A {
+    constructor(name) {
+        this.name = name
+    }
+
+    f1() {
+        console.log(`${this.name} has function 1!`)
+    }
+
+    f2() {
+        console.log(`${this.name} has function 2!`)
+    }
+}
+
+let original = new A("original");
+
+original.f1()
+original.f2()
+
+delete A.prototype.f2
+
+console.log()
+
+let mutant = new A("mutant");
+
+mutant.f1()
+try {
+    mutant.f2()
+} catch (e) {
+    console.log(e.message)
+}
+
+console.log()
+
+if (typeof original === typeof mutant) {
+    console.log("Types are still equal after modifying the prototype! How cool!")
+}
